e83529cd559a273292b1b72e8e87b8cbec94f8a6
cepedagm14 <cepedagm14@gmail.com>
2022-06-19 23:16:04 -0400

    using countup library

--- toolTip-counTup/src/App.jsx
+++ toolTip-counTup/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import { Tooltip } from "react-tippy";
 import "react-tippy/dist/tippy.css";
+import CountUp, { useCountUp } from "react-countup";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const countUpRef = React.useRef(null);
+  const { start, pauseResume, reset, update } = useCountUp({
+    ref: countUpRef,
+    start: 0,
+    duration: 5,
+    end: 10000,
+  });
 
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <div ref={countUpRef} />
+          <button onClick={start}>Start</button>
+          <button onClick={reset}>Reset</button>
+          <button onClick={pauseResume}>Pause/Resume</button>
+          <button onClick={() => update(2000)}>Update to 2000</button>
+        </div>
+
         <Tooltip
           title="Welcome to React"
           position="left"
@@ -18,6 +32,12 @@ function App() {
         >
           <button>ToolTips</button>
         </Tooltip>
+
+        {/* countup component */}
+
+        {/* <CountUp end={200} duration={5.75}></CountUp>
+        <CountUp end={200} duration={5.75} prefix="$"></CountUp>
+        <CountUp end={200} duration={5.75} suffix="CLP" decimals={2}></CountUp> */}
       </header>
     </div>
   );
